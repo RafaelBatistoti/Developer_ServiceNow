95be89f6c4a60c92124d4c9b9e8b1d3fef574cc9
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2024-05-03 13:31:51 -0300

    Some examples and use case about GlideUser API

---
+++ GlideUser/GlideUser.js
@@ -0,0 +1,15 @@
+alert('Hi! My name is: ' + g_user.firstName + '\nmy last name is: ' + g_user.lastName + '\nmy sys_id is: ' + g_user.userID);
+
+//*******************************************************
+
+alert('My full name is: ' + g_user.fullName)
+
+//*******************************************************
+
+alert('Do you have a roles? ' + g_user.hasRoles())
+
+//*******************************************************
+
+alert('My user name is: ' + g_user.userName);
+
+//*******************************************************
